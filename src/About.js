a8d68460ed1fabfe03fc594a28e808a26f9db3cf
Wynder <86427638+cosonlu@users.noreply.github.com>
2022-07-18 17:10:20 -0700

    Added jquery, revamped about me sections (added and animated images, made format into grid instead of flex [to compensate for the images]), added scroll to top feature on nav bar click, added typed.js for typing feature on top of page

--- src/About.js
+++ src/About.js
@@ -1,92 +1,104 @@
 import React  from 'react';
 import "./general.css"
 import "./about.css"
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { useEffect } from "react";
 
 function About() {
     
-    gsap.registerPlugin(ScrollTrigger);
+    // gsap.registerPlugin(ScrollTrigger);
     
 
 
-    useEffect(() => {
-        const intItems = gsap.utils.toArray('.ab-sect-cont')
-        const int = gsap.utils.toArray('.interest')
-        intItems.forEach(box => {
-            gsap.from(box, {
-                scrollTrigger: {
-                    trigger: box,
-                    start: "top 95%",
-                    toggleActions: "restart none restart pause" 
-                },
-                x: 60,
-                opacity: 0,
-                duration: 0.4,
-                ease: "power4.out"
-            })
-        });
+    // useEffect(() => {
+    //     const intItems = gsap.utils.toArray('.ab-sect-cont')
+    //     const int = gsap.utils.toArray('.interest')
+    //     intItems.forEach(box => {
+    //         gsap.from(box, {
+    //             scrollTrigger: {
+    //                 trigger: box,
+    //                 start: "top 95%",
+    //                 toggleActions: "restart none restart pause" 
+    //             },
+    //             x: 60,
+    //             y: 20,
+    //             opacity: 0,
+    //             duration: 0.4,
+    //             ease: "power4.out"
+    //         })
+    //     });
         
-        int.forEach(boxi => {
-            gsap.from(boxi, {
-                scrollTrigger: {
-                    trigger: boxi,
-                    start: "top 95%",
-                    toggleActions: "restart none restart pause" 
-                },
-                x: -60,
-                opacity: 0,
-                duration: 0.4,
-                ease: "power4.out"
-            })
-        });
-    });
+    //     int.forEach(boxi => {
+    //         gsap.from(boxi, {
+    //             scrollTrigger: {
+    //                 trigger: boxi,
+    //                 start: "top 95%",
+    //                 toggleActions: "restart none restart pause" 
+    //             },
+    //             x: -60,
+    //             y: 20,
+    //             opacity: 0,
+    //             duration: 0.4,
+    //             ease: "power4.out"
+    //         })
+    //     });
+    // });
 
     return (
         <>
             <div class="content">
+                <h1 class="ab-header">Interests And Hobbies</h1>
                 <div class="abt-card-cont">
+                    <Card
+                    interest="Coding"
+                    interestContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eu ultrices vitae auctor. Erat imperdiet sed euismod nisi porta lorem. Bibendum arcu vitae elementum curabitur. Id cursus metus aliquam eleifend mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Amet facilisis magna etiam tempor orci"
+                    gray="True"
+                    imgs="forest.jpg"
+                    />
+                    <Card
+                    interest="2"
+                    interestContent="Coding"
+                    gray="False"
+                    imgs="forest.jpg"
+                    />
                     <Card
                     interest="Coding"
                     interestContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eu ultrices vitae auctor. Erat imperdiet sed euismod nisi porta lorem. Bibendum arcu vitae elementum curabitur. Id cursus metus aliquam eleifend mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Amet facilisis magna etiam tempor orci. Luctus accumsan tortor posuere ac ut consequat semper. Auctor urna nunc id cursus metus aliquam eleifend. Et netus et malesuada fames. Ornare lectus sit amet est placerat. Tortor aliquam nulla facilisi cras fermentum odio eu. Aenean sed adipiscing diam donec. Elementum sagittis vitae et leo duis ut."
                     gray="True"
+                    imgs="forest.jpg"
                     />
                     <Card
                     interest="2"
                     interestContent="Coding"
                     gray="False"
+                    imgs="forest.jpg"
                     />
                     <Card
-                    interest="1"
+                    interest="Coding"
                     interestContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eu ultrices vitae auctor. Erat imperdiet sed euismod nisi porta lorem. Bibendum arcu vitae elementum curabitur. Id cursus metus aliquam eleifend mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Amet facilisis magna etiam tempor orci. Luctus accumsan tortor posuere ac ut consequat semper. Auctor urna nunc id cursus metus aliquam eleifend. Et netus et malesuada fames. Ornare lectus sit amet est placerat. Tortor aliquam nulla facilisi cras fermentum odio eu. Aenean sed adipiscing diam donec. Elementum sagittis vitae et leo duis ut."
                     gray="True"
+                    imgs="forest.jpg"
                     />
                     <Card
                     interest="2"
                     interestContent="Coding"
                     gray="False"
+                    imgs="forest.jpg"
                     />
                     <Card
-                    interest="1"
+                    interest="Coding"
                     interestContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eu ultrices vitae auctor. Erat imperdiet sed euismod nisi porta lorem. Bibendum arcu vitae elementum curabitur. Id cursus metus aliquam eleifend mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Amet facilisis magna etiam tempor orci. Luctus accumsan tortor posuere ac ut consequat semper. Auctor urna nunc id cursus metus aliquam eleifend. Et netus et malesuada fames. Ornare lectus sit amet est placerat. Tortor aliquam nulla facilisi cras fermentum odio eu. Aenean sed adipiscing diam donec. Elementum sagittis vitae et leo duis ut."
                     gray="True"
+                    imgs="forest.jpg"
                     />
                     <Card
                     interest="2"
                     interestContent="Coding"
                     gray="False"
+                    imgs="forest.jpg"
                     />
-                    <Card
-                    interest="1"
-                    interestContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eu ultrices vitae auctor. Erat imperdiet sed euismod nisi porta lorem. Bibendum arcu vitae elementum curabitur. Id cursus metus aliquam eleifend mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Amet facilisis magna etiam tempor orci. Luctus accumsan tortor posuere ac ut consequat semper. Auctor urna nunc id cursus metus aliquam eleifend. Et netus et malesuada fames. Ornare lectus sit amet est placerat. Tortor aliquam nulla facilisi cras fermentum odio eu. Aenean sed adipiscing diam donec. Elementum sagittis vitae et leo duis ut."
-                    gray="True"
-                    />
-                    <Card
-                    interest="2"
-                    interestContent="Coding"
-                    gray="False"
-                    />
+                    
                 </div>
                 
             </div>
@@ -99,8 +111,11 @@ function Card(props) {
         return (
             <>
                 <div class="gray ab-sect-card">
-                    <p class="interest">{props.interest}</p>
-                    <p class="ab-sect-cont">{props.interestContent}</p>
+                    <p class="interest grid-item">{props.interest}</p>
+                        <div class="pic-item grid-item">
+                            <img class="pic" src={props.imgs} alt=""></img>
+                        </div>
+                    <p class="ab-sect-cont grid-item">{props.interestContent}</p>
                 </div>
             </>
         )
@@ -108,8 +123,11 @@ function Card(props) {
         return (
             <>
                 <div class="ab-sect-card">
-                    <p class="interest">{props.interest}</p>
-                    <p class="ab-sect-cont">{props.interestContent}</p>
+                    <p class="interest grid-item">{props.interest}</p>
+                        <div class="pic-item grid-item">
+                            <img class="pic" src={props.imgs} alt=""></img>
+                        </div>
+                    <p class="ab-sect-cont grid-item">{props.interestContent}</p>
                 </div>
             </>
         )
--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import {Outlet, Link} from "react-router-dom";
 import React  from 'react';
 import "./nav.css";
 import "./general.css";
-
+import Typed from 'typed.js';
 
 
 function App() {
-
+  React.useEffect(() => {
+    var data = {
+      strings: ["Programming enthusiast", "Soccer Player", "Middle Schooler", "Chess Professional", "Sleep Ninja"],
+      shuffle: true,
+      backDelay: 1600,
+      loop: true,
+      loopCount: Infinity,
+      typeSpeed: 50,
+      backSpeed: 20,
+    };
+    new Typed('#animated-text', data);
+  })
 
   return (
     <>
       <h1 class="header">Coson Lu</h1>
+      <p id="quote"><span id="animated-text"></span></p>
       <nav>
-        <Link to="/" class="nav-item">About Me</Link>
-        <Link to="/blog" class="nav-item">Blog</Link>
+        <Link to="/" class="nav-item"  onClick={() => {
+          window.scroll(0, 0)
+        }}>About Me</Link>
+        <Link to="/blog" class="nav-item" onClick={() => {
+          window.scroll(0, 0)
+        }}>Blog</Link>
       </nav>
       <Outlet />
     </>
